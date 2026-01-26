54219826397044cce04075227066b810aa45c4af
Brigitte Blau <brigitteyaelblau@gmail.com>
2026-01-26 13:28:03 -0300

    Add immersive three.js hero with vision controls

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,11 +5,13 @@ import Hero from './components/Hero';
 import Work from './components/Work';
 import PlanetIntro from './components/PlanetIntro';
 import Story from './components/Story';
- import Contact from './components/Contact';
+import Contact from './components/Contact';
+import ImmersiveScene from './components/ImmersiveScene';
 
 function App() {
   return (
     <div className="min-h-screen relative text-neutral-900 font-sans bg-white overflow-hidden">
+      <ImmersiveScene />
       <PlanetIntro />
       <div className="absolute top-[-200px] left-1/2 -translate-x-1/2 w-[600px] h-[600px] bg-pink-300 rounded-full blur-3xl opacity-30 z-0 pointer-events-none"></div>
       <div className="relative z-10">
