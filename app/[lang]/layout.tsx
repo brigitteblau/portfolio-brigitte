d8d8fb849691c0bb83518e27d7ba7f425f657501
Brigitte Blau <brigitteyaelblau@gmail.com>
2026-06-28 00:07:40 -0300

    lol

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,13 +1,7 @@
 export default function LangLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: { lang: 'es' | 'en' };
 }) {
-  return (
-    <html lang={params.lang}>
-      <body>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -9,19 +9,25 @@ import Journal from '@/components/Journal';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 
-export default async function Home({ params }: any) {
-  const dict = getDictionary(params.lang);
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ lang: 'es' | 'en' }>;
+}) {
+  const { lang } = await params;
+
+  const dict = getDictionary(lang);
   const [projects, posts] = await Promise.all([getProjects(), getPosts()]);
 
   return (
     <>
-      <SideNav dict={dict} lang={params.lang} />
+      <SideNav dict={dict} lang={lang} />
       <main>
         <Hero dict={dict} />
-        <Work dict={dict} projects={projects} lang={params.lang} />
+        <Work dict={dict} projects={projects} lang={lang} />
         <About dict={dict} />
         <Studies dict={dict} />
-        <Journal dict={dict} posts={posts} lang={params.lang} />
+        <Journal dict={dict} posts={posts} lang={lang} />
         <Contact dict={dict} />
       </main>
       <Footer dict={dict} />
